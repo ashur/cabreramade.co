c2a5a18ddac0c1532be00841dc7bfecfe7435d1e
Ashur Cabrera <hello@cabreramade.co>
2017-12-27 20:02:16 -0800

    🥤 Refactoring template task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,67 @@
 'use strict';
 
-const fs         = require( 'fs' );
-const gulp       = require( 'gulp' );
-const handlebars = require( 'gulp-compile-handlebars' );
-const layouts    = require( 'handlebars-layouts' );
-const rename     = require( 'gulp-rename' );
-const yaml       = require( 'js-yaml' );
+const fs          = require( 'fs' );
+const glob        = require( 'glob' );
+const gulp        = require( 'gulp' );
+const hb          = require( 'gulp-hb' );
+const layouts     = require( 'handlebars-layouts' );
+const helpers     = require( 'handlebars-helpers' );
+const path        = require( 'path' );
+const rename      = require( 'gulp-rename' );
+const set         = require( 'set-value' );
+const smartquotes = require( 'smartquotes' );
+const yaml        = require( 'js-yaml' );
 
 var paths = {
 	src: "src",
 	dist: "dist",
 
+	data: "src/data",
+	partials: "src/partials",
 	templates: "src/templates",
 };
 
-gulp.task('templates', function()
+gulp.task( 'templates', function()
 {
-	var siteData = yaml.safeLoad( fs.readFileSync( 'data.yml', 'utf8' ) );
+	var hbStream = hb()
+		.partials( `${paths.partials}/**/*.hbs` )
 
-	return gulp.src( `${paths.templates}/**/*.hbs` )
-		.pipe( handlebars( siteData, {} ) )
-		.pipe( rename( function( path )
+		.helpers( layouts )
+		.helpers( helpers.markdown )
+		.helpers({
+			smartquotes: function( options )
+			{
+				return smartquotes( options.fn( this ) );
+			}
+		})
+
+	/* Convert YAML to single data object */
+	glob( `**/*.yml`, { cwd: paths.data }, function( er, files )
+	{
+		var data = {};
+
+		files.forEach( function( filename )
 		{
-			path.extname = '.html';
-		}))
-		.pipe( gulp.dest( paths.dist ) );
+			var propertyPath = filename
+				.substring( 0, filename.indexOf( '.' ) )
+				.split( '/' )
+				.join( '.' );
+
+			var fileData = yaml.safeLoad( fs.readFileSync( `${paths.data}/${filename}`, 'utf8' ) );
+
+			set( data, propertyPath, fileData );
+		});
+
+		hbStream.data( data );
+
+		return gulp
+	        .src( `${paths.templates}/**/*.hbs` )
+	        .pipe( hbStream )
+			.pipe( rename( function( path )
+			{
+				path.extname = '.html';
+			}))
+			.pipe( gulp.dest( paths.dist ) );
+	});
+});
 });
